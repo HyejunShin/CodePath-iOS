bbe28a4232a13ff9a6effbd0b37e9a3c6d1502c5
HyejunShin <92062760+HyejunShin@users.noreply.github.com>
2022-03-16 05:37:22 -0400

    unit5

--- unit5/Parstagram/Parstagram/FeedViewController.swift
+++ unit5/Parstagram/Parstagram/FeedViewController.swift
@@ -12,14 +12,60 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    let myRefreshControl = UIRefreshControl()
     var posts = [PFObject]()
+    var numberOfPosts: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadPosts()
+        myRefreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
+    }
+    
+    @objc func loadPosts(){
+        numberOfPosts = 20
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+        run(after: 1) {
+            self.myRefreshControl.endRefreshing()
+        }
+    }
+    
+    func loadMorePosts(){
+        numberOfPosts += 10
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    //when user gets to the end of page, run loadMorePosts()
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            loadMorePosts()
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,7 +73,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let query = PFQuery(className:"Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = numberOfPosts
         query.findObjectsInBackground{(posts, error) in
             if posts != nil {
                 self.posts = posts!
